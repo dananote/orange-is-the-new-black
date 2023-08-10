60b59d118be8d59a1614ceaeb68af6d19656bc55
taehee.rho <taeheerho@gmail.com>
2023-08-11 01:24:07 +0900

    Feat: next button 기능 구현

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
 import Nav from "../components/Nav";
@@ -7,15 +7,33 @@ import LayoutPC from "./LayoutPC";
 import LayoutMobile from "./LayoutMobile";
 import { NextBtn, SoundImageWrapper } from "./LayoutStyle";
 import lpImage from "../asset/common/soundtrack.png";
+import { PageData } from "../mock/menuList";
+
+interface PageType {
+  page: string;
+  path: string;
+}
 
 const Layout = () => {
   const isDesktop = useMediaQuery({ query: "(min-width:800px)" });
   const isMobile = useMediaQuery({ query: "(max-width:799px)" });
   const location = useLocation();
+  const navigate = useNavigate();
   const [showNav, setShowNav] = useState<boolean>(false);
 
   const handleMenu = () => {
     setShowNav((prev) => !prev);
+  };
+
+  const handleNext = () => {
+    const currentPage = location.pathname;
+    const index = PageData.findIndex((el) => el.path === currentPage);
+
+    if (PageData.length - 1 === index) {
+      navigate("/");
+    } else {
+      navigate(PageData[index + 1].path);
+    }
   };
 
   return (
@@ -36,7 +54,7 @@ const Layout = () => {
           handleMenu={handleMenu}
         />
       )}
-      <NextBtn isMobile={isMobile} />
+      <NextBtn isMobile={isMobile} onClick={handleNext} />
       <SoundImageWrapper>
         <img src={lpImage} alt="Lp이미지" />
       </SoundImageWrapper>
